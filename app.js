4b076404c3758143d30c7fd3ae22b6ebc4540254
Kaushal Prakash <kingkaushal1289@gmail.com>
2024-05-24 23:57:52 +0530

    salting and hashing

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -37,38 +39,42 @@ app.get("/secrets",(req,res)=>{
 });
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save()
+            .then(() => {
+                // User saved successfully
+                res.render("secrets");
+            })
+            .catch((err) => {
+                // Handle the error
+                console.error(err);
+                // You can also send an error response to the client if needed
+                res.status(500).send("Internal server error");
+            });
     });
 
-    newUser.save()
-        .then(() => {
-            // User saved successfully
-            res.render("secrets");
-        })
-        .catch((err) => {
-            // Handle the error
-            console.error(err);
-            // You can also send an error response to the client if needed
-            res.status(500).send("Internal server error");
-        });
+    
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const pass = md5(req.body.password);
+    const pass = req.body.password; 
 
     User.findOne({ email: username })
         .then((founduser) => {
             if (founduser) {
-                // Compare hashed passwords (use bcrypt or similar)
-                // For demonstration purposes, I'm assuming plain text comparison
-                if (founduser.password === pass) {
-                    res.render("secrets");
-                } else {
-                    res.status(401).send("Incorrect password");
-                }
+                bcrypt.compare(pass, founduser.password, function(err, result) {
+                    if(result==true){
+                        res.render("secrets");
+                    }else {
+                        res.status(401).send("Incorrect password");
+                    }
+                });
             } else {
                 res.status(404).send("User not found");
             }
